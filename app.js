c83731c029665411310c71cce8eb44b758b34a15
@marcelogiaccaglia <marcelogiaccaglia@gmail.com>
2020-07-19 12:29:36 -0300

    modifico ruta de hoja de estilos - creo carpeta Public que ahora contiene a carpeta css y a su vez esta contiene el archivo styles.css - renombro el archivo style por styles.css - creo la propiedad use en app.js para poder usar la funcion de express.static hacia la carpeta public que contendra archivos estaticos - modifico ruta de hoja de estilo en todas las rutas html creadas

--- app.js
+++ app.js
@@ -2,39 +2,39 @@
 const express = require('express');
 const app = express();
 
-//iniciar en 3000
-app.listen(3000, ()  => {console.log('Servidor escuchando en el puerto 3000.')});
+//crear funcion de express para utilizar carpeta public para archivos estaticos
+app.use(express.static("public"));
 
 //crear ruta para raiz '/'
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/views/index.html');
-} )
+});
 
 //crear ruta '/productos'
 app.get('/productos', function(req, res){
     res.sendFile(__dirname + '/views/productos.html');
-} )
+});
 
 //crear ruta '/acerca-de'
 app.get('/acerca-de', function(req, res){
     res.sendFile(__dirname + '/views/acerca-de.html');
-} )
+});
 
 //crear ruta '/contacto'
 app.get('/contacto', function(req, res){
     res.sendFile(__dirname + '/views/contacto.html');
-} )
+});
 
 //crear ruta '/boom' 
 app.get('/boom', function(req, res){
     res.sendFile(__dirname + '/views/boom.html');
-} )
-
-app.get('/style', function(req, res){
-    res.sendFile(__dirname + '/css/style.css');
-} )
+});
 
 //crear ruta de _inexistente_
 app.get('*', function(req, res){
     res.status(404).sendFile(__dirname + '/views/404.html');
-} )
+});
+
+
+//iniciar en 3000
+app.listen(3000, ()  => {console.log('Servidor escuchando en el puerto 3000.')});
