0a95d7649554af14b93f13b1525fd12b69bc45d2
@marcelogiaccaglia <marcelogiaccaglia@gmail.com>
2020-09-21 22:27:33 -0300

    Agrgado de archivo .txt Lista a Mejorar, contenedor de files de Multer fileProducts y unificacion de lenguaje en ingles en app.js

--- site/routes/productos_route.js
+++ site/routes/productos_route.js
@@ -12,7 +12,7 @@ const multer = require('multer');
 let path = require('path');
 var storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, './public/products')
+        cb(null, './public/fileProducts')
     },
     filename: function(req, file, cb){
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
