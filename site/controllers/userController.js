2090955856d58e66ac970ed2bc54473997c56e8a
TiagoAltstadt <tiagoaltstadt@gmail.com>
2020-09-25 15:01:14 -0300

    -agregue metodo de mysql a la parte de edicion de usuarios -modificando la seccion de lista de usuarios -elimine una propiedad general del body (width: 80% y margin: auto 10%;)

--- site/controllers/userController.js
+++ site/controllers/userController.js
@@ -15,6 +15,7 @@ const userController = {
 
             db.Users.findAll()
                 .then(function (users) {
+
                     res.render('../views/users/list_user.ejs', { users: users });
                 })
 
@@ -38,37 +39,39 @@ const userController = {
                 password: bcrypt.hashSync(req.body.password, 10),
                 category: 1
             })
-            .then(user => {
-                res.redirect('/');
-            })
-            
+                .then(user => {
+                    res.redirect('/');
+                })
+
 
         },
     login: function (req, res) {
-            //envio la vista
-            res.render('users/login');
-        },
+        let password = bcrypt.hashSync('mono@gmail.com', 10);
+        console.log(bcrypt.compareSync('mono@gmail.com', '$2a$10$cuGGOcbTI4pc2KUZwX5.v.CD0XuzxQw3ME5p1IXh1RA'));
+
+        res.render('users/login');
+    },
     processLogin: function (req, res) {
 
 
         //  Pido buscar en la db si hay coincidencia
         db.Users.findOne({ where: { email: req.body.email } })
-            .then( user => {
+            .then(user => {
 
                 //  De ser nulo el valor (no hay coincidencia) retorno un console.log que diga que esta mal
                 if (user) {
-                    
+
                     console.log('El Email esta bien.');
-                    console.log('Contraseña: ' , user.password);
+                    console.log('Contraseña: ', user.password);
                     console.log('Contraseña body: ', req.body.password);
                     //   Corroboro la password 
                     if (bcrypt.compareSync(req.body.password, user.password)) {
 
                         console.log('La password esta bien.');
-                        
+
                         userData = user.dataValues;
                         delete userData.password
-        
+
                         req.session.user = userData;
 
                     } else {
@@ -76,7 +79,7 @@ const userController = {
                         console.log('La password esta mal.');
                     }
 
-                } else { 
+                } else {
                     console.log('Email Invalido.');
                 }
             })
@@ -96,80 +99,46 @@ const userController = {
                 })
 
         },
-    search:
-        function (req, res) {
+    search: function (req, res) {
 
-            let aux = req.params.id;
+        let aux = req.params.id;
 
-            db.Users.findByPk(aux)
-                .then(function (user) {
-                    res.render('usersResults.ejs', { usersResults: user });
-                })
-
-
-        },
-    edit: function (req, res) {
-
-        //leo el json de usuarios y lo paso a la variable archivoUsuarios como objeto literal
-        const archivoUsuarios = fs.readFileSync('./data/users.json', { encoding: 'utf-8' });
-        const userJSON = JSON.parse(archivoUsuarios);
-
-        //creo la variable data y le paso la info del url para que sepa de que usuario estamos hablando
-        let data = req.params.id - 1;
-
-        //renderizo la vista y le envio users, que contiene el json con la base de datos + la variable data que va a definir cual usuario mostrar en los campos a completar
-        res.render('./users/edicion_users.ejs', { 'users': userJSON, 'data': data });
-    },
-    update:
-        function (req, res) {
-            //guardo la id en una variable
-            let idUser = parseInt(req.params.id) + 1;
-
-            //guardo todos los datos del formulario en la variable user
-            const user = {
-                id: req.params.id,
-                avatar: req.body.avatar,
-                user: req.body.user,
-                name: req.body.name,
-                surname: req.body.surname,
-                email: req.body.email,
-                password: req.body.password,
-                category: req.body.category
-            }
-
-            //leo el json de usuarios y lo paso a la variable archivoUsuarios como objeto literal
-            const archivoUsuarios = fs.readFileSync('./data/users.json', { encoding: 'utf-8' });
-            const users = JSON.parse(archivoUsuarios);
-
-            let updated = users.map(function (update) {
-                if (update.id == idUser) {
-                    let aux = update;
-                    aux.id = user.id;
-                    aux.avatar = user.avatar;
-                    aux.user = user.user;
-                    aux.name = user.name;
-                    aux.surname = user.surname;
-                    aux.email = user.email;
-                    aux.password = user.password;
-                    category = user.category
-
-                    return user;
-                }
-                else {
-                    return update;
-                }
+        db.Users.findByPk(aux)
+            .then(function (user) {
+                res.render('usersResults.ejs', { usersResults: user });
             })
 
-            //lo vuelvo un string para guardarlo
-            const usersJSON = JSON.stringify(updated, null, " ");
 
-            //uso write para pisar la data previa y guardar todo lo nuevo
-            fs.writeFileSync('./data/users.json', usersJSON);
+    },
+    edit: function (req, res) {
 
-            //redirecciono
-            res.redirect('/');
+        db.Users.findOne({ where: { id: req.params.id } })
+            .then(function (user) {
+                res.render('./users/edicion_users.ejs', { user: user, data: req.params.id });
+            })
 
-        }
+
+
+    },
+    update: function (req, res) {
+        db.Users.update({
+            avatar: req.body.avatar,
+            username: req.body.username,
+            name: req.body.name,
+            surname: req.body.surname,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10),
+            category: 1
+
+        },
+            {
+                where: {
+                    email: req.params.email
+                }
+            })
+        res.redirect('/');
+
+    }
 };
 
 //----Exports----
