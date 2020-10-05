f264515f44db6ed430a41e7593c7b026d8dca57a
@marcelogiaccaglia <marcelogiaccaglia@gmail.com>
2020-10-05 03:33:27 -0300

    Creacion de nueva estructura MySQL segun diagrama DER

--- otros/DAILY.md
+++ otros/DAILY.md
@@ -11,3 +11,13 @@
 23-09-2020 (Sprint 7)
 
 En el dia indicado nos reunimos para debatir cambios en el front del Home, se plantearon dudas respecto al funcionamiento de Session donde utilizaremos la clase de colerning para lograr evacuar dudas. Tambien se planteo los objetivos a corto plazo que debemos afrontar cada uno para ponernos al dia con los requisitos solicitados en los Sprint anteriores (Objetivos Midelware, Session y MySQL)
+
+----------------------------------------------------------------------------
+25-09-2020 (Sprint 8)
+
+En el dia de la fecha nos reunimos para evacuar dudas respecto a los Middelware a implementar, tambien las consultas a realizar para la clase especial de ayuda.
+
+----------------------------------------------------------------------------
+01-10-2020
+
+En el dia de la fecha realizamos una reunion para vizualizar avances respecto a las implementaciones de session y todo lo referido a la base de datos con MySQL, nuestro objetivo es terminar de implementar base de datos a la brevedad 
---
+++ site/database/baggu.sql
@@ -0,0 +1,97 @@
+CREATE DATABASE  IF NOT EXISTS `baggu` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
+USE `baggu`;
+
+-- MariaDB dump 10.17  Distrib 10.4.13-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: baggu
+-- ------------------------------------------------------
+-- Server version	10.4.13-MariaDB
+
+-- Table structure for table `brands`
+--
+
+DROP TABLE IF EXISTS `brands`;
+CREATE TABLE `brands` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+
+--
+-- Table structure for table `category_users`
+--
+
+DROP TABLE IF EXISTS `category_users`;
+CREATE TABLE `category_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `description` varchar(400) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `price` decimal(6,2) NOT NULL,
+  `image_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `image_foreign_idx` (`image_id`),
+  KEY `brand_foreign_idx` (`brand_id`),
+  KEY `category_foreign_idx` (`category_id`),
+  CONSTRAINT `brand_foreign` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `category_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `image_foreign` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `avatar` varchar(200) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `username` varchar(100) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `surname` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `email` varchar(250) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `password` varchar(300) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `category_user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `category_user_foreign_idx` (`category_user_id`),
+  CONSTRAINT `category_user_foreign` FOREIGN KEY (`category_user_id`) REFERENCES `category_users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Dumping routines for database 'baggu'
